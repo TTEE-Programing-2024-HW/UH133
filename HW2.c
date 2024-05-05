db45651a8c49f38243c3a4ce171346bafa569c26
u11219033 <168330686+u11219033@users.noreply.github.com>
2024-05-05 23:06:10 +0800

    Update HW2.c
    寫出主選單，將變數i，j寫到最前面並宣告
    寫出三角形的排列

--- HW2.c
+++ HW2.c
@@ -1,34 +1,35 @@
-//修改個人介面,以及修正c99，c11的版本問題，比如在for迴圈前，要先宣告變數
-//寫了警告提示字句
+//寫出主選單，將變數i，j寫到最前面並宣告
+//寫出三角形的排列
 
 
 
 #include <stdio.h>
 #include <stdlib.h>
-
-void display_screen() {
 int i;
 int j;
-for (i = 0; i < 20; i++) {
+// 顯示個人風格的畫面畫出上邊界
+void display_screen() {
+for ( i = 0; i < 20; i++) {
 printf("*");
 }
 printf("\n歡迎來到個人風格的畫面!\n");
-for (i = 0; i < 20; i++) {
+for ( i = 0; i < 20; i++) {
 printf("*");
 }
 printf("\n");
-for (i = 0; i < 17; i++) {
+for ( i = 0; i < 17; i++) {
 printf("*");
-for (j = 0; j < 18; j++) {
+for ( j = 0; j < 18; j++) {
 printf(" ");
 }
 printf("*\n");
 }
-for (i = 0; i < 20; i++) {
+for ( i = 0; i < 20; i++) {
 printf("*");
 }
 printf("\n");
 }
+// 檢查密碼是否正確
 int check_password() {
 int correct_password = 2024;
 int attempts = 0;
@@ -47,3 +48,47 @@ printf("密碼錯誤，您已經嘗試了 %d 次。\n", attempts);
 printf("警告：您已經連續錯誤三次，程式將結束。\n");
 return 0;
 }
+// 畫出直角三角形
+void draw_triangle(char c) {
+int size = c - 'a' + 1;
+for ( i = 0; i < size; i++) {
+for ( j = 0; j <= i; j++) {
+printf("%c ", c);
+}
+printf("\n");
+}
+}
+void draw_square(char c) {
+int size = c - 'a' + 1;
+for ( i = 0; i < size; i++) {
+for ( j = 0; j < size; j++) {
+printf("%c ", c);
+}
+printf("\n");
+}
+}
+// 顯示乘法表
+void print_multiplication_table(int n) {
+for ( i = 1; i <= n; i++) {
+for ( j = 1; j <= n; j++) {
+printf("%d * %d = %d\t", i, j, i * j);
+}
+printf("\n");
+}
+}
+// 主函數
+int main() {
+display_screen();
+if (!check_password()) {
+return 0;
+}
+// 進入主選單
+while (1) {
+printf("--------------------------\n");
+printf("|  a. 畫出直角三角形\n");
+printf("|  b. 顯示乘法表\n");
+printf("|  c. 結束\n");
+printf("--------------------------\n");
+char option;
+printf("請選擇一個選項: ");
+scanf(" %c", &option);
